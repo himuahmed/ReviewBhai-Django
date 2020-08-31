c73de1675c550fb8eb605d4e35f368cdef7d4357
Himu Ahmed <34248175+himuahmed@users.noreply.github.com>
2020-08-31 12:00:36 +0600

    Update settings.py

--- reviewbhai/reviewbhai/settings.py
+++ reviewbhai/reviewbhai/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'emailserverd@gmail.com'
-EMAIL_HOST_PASSWORD = 'btaydqarcndkvchg'
+EMAIL_HOST_PASSWORD = ''
