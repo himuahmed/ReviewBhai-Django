65c871472af7b302aaa96a48b9052ac5e07dce76
Himu <Himu@DESKTOP-KSVV9J9>
2020-09-07 03:02:58 +0600

    Changed Review Crad Design !!

--- reviewbhai/reviews/admin.py
+++ reviewbhai/reviews/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from.models import Review,Tag,Item
 # Register your models here.
 admin.site.register(Review)
-admin.site.register(Tag)
-admin.site.register(Item)
+
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name','type','food_or_travel','date_added')
+    search_fields = ('name','type','food_or_travel','date_joined')
+    read_only = ('date_added')
+
+    fieldsets = ()
+    filter_horizontal = ()
+    list_filter = ()
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag_name','foods_or_travel','date_added')
+    search_fields = ('tag_name','foods_or_travel','date_joined')
+    read_only = ('date_added')
+
+    fieldsets = ()
+    filter_horizontal = ()
+    list_filter = ()
+
+
+
+admin.site.register(Tag,TagAdmin)
+admin.site.register(Item,ItemAdmin)
